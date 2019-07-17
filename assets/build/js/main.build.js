dbaf523eebb1798eaf4b5480e84e5f8a7bee5ac9
Shubham Mishra <shubhammishra9159@gmail.com>
2019-07-17 18:01:24 +0530

    V 1.0

--- assets/build/js/main.build.js
+++ assets/build/js/main.build.js
@@ -1,3 +1,28 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/javascript");
+// var editor = ace.edit("editor");
+// editor.setTheme("ace/theme/monokai");
+// editor.session.setMode("ace/mode/javascript");
+
+// dropdown class
+
+function dropdown_menu() {
+    $('.dd-nav li').on('click', function () {
+        $(this).parents('.parent-dd').find('.child-dd').css('height', 'auto');
+    });
+}
+
+function functionSequence() {
+    dropdown_menu();
+}
+
+// =========================================================
+// On Load
+// =========================================================
+if (window.addEventListener) {
+    window.addEventListener('load', function () {
+        functionSequence();
+    });
+} else {
+    window.attachEvent('onload', function () {
+        functionSequence();
+    });
+}
